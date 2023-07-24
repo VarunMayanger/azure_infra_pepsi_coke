b33a4700b5f50a321aa612039feba317910b482e
VarunMayanger <mrmayanger@gmail.com>
2023-07-24 12:20:59 +0530

    correction in principal object id

--- modules/service_principal/main.tf
+++ modules/service_principal/main.tf
@@ -40,7 +40,7 @@ resource "azuread_service_principal" "msgraph" {
 resource "azurerm_role_assignment" "main" {
   scope                = "/subscriptions/95657feb-1223-4d45-938f-fe10eadfa20e"
   role_definition_name = "Owner"
-  principal_id         = azuread_service_principal.main..service_principal_object_id
+  principal_id         = azuread_service_principal.main.service_principal_object_id
   app_role_id         = azuread_service_principal.msgraph.app_role_ids["User.Read.All"]
   principal_object_id = azuread_service_principal.main.object_id
   resource_object_id  = azuread_service_principal.msgraph.object_id   

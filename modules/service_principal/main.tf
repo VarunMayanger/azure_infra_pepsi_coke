21fedeace55950d3ab6a32adad992599fbb9fc02
VarunMayanger <mrmayanger@gmail.com>
2023-07-24 15:06:59 +0530

    empty

--- modules/service_principal/main.tf
+++ modules/service_principal/main.tf
@@ -1,3 +1,4 @@
+
 data "azuread_application_published_app_ids" "well_known" {}
 
 resource "azuread_service_principal" "msgraph" {
@@ -12,17 +13,23 @@ resource "azuread_application" "example" {
     resource_app_id = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
 
     resource_access {
-      id   = azuread_service_principal.msgraph.app_role_ids["Directory.ReadWrite.All"]
+      id   = azuread_service_principal.msgraph.app_role_ids["User.Read.All"]
       type = "Role"
     }
 
     resource_access {
-      id   = azuread_service_principal.msgraph.oauth2_permission_scope_ids["Directory.ReadWrite.All"]
+      id   = azuread_service_principal.msgraph.oauth2_permission_scope_ids["User.ReadWrite"]
       type = "Scope"
     }
   }
 }
 
-resource "azuread_service_principal" "main" {
+resource "azuread_service_principal" "example" {
   application_id = azuread_application.example.application_id
+}
+
+resource "azuread_app_role_assignment" "example" {
+  app_role_id         = azuread_service_principal.msgraph.app_role_ids["Application.ReadWrite.All"]
+  principal_object_id = azuread_service_principal.example.object_id
+  resource_object_id  = azuread_service_principal.msgraph.object_id
 }

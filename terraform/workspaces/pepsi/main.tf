8c5c5076b56ae6c3840897de323d1476f08fc365
VarunMayanger <mrmayanger@gmail.com>
2023-08-01 20:03:31 +0530

    with correction in local variables

--- terraform/workspaces/pepsi/main.tf
+++ terraform/workspaces/pepsi/main.tf
@@ -67,11 +67,11 @@ module "security_group" {
   security_rule_source_address_prefix      = "*"
   security_rule_destination_port_range     = "*"
   security_rule_source_port_range          = "*"
-  security_rule_protocol                   = local.sa_security_protocol
-  security_rule_access                     = local.sa_security_rule_access
-  security_rule_direction                  = local.sa_security_rule_direction
+  security_rule_protocol                   = local.sg_security_protocol
+  security_rule_access                     = local.sg_security_rule_access
+  security_rule_direction                  = local.sg_security_rule_direction
   security_rule_priority                   = 100
-  security_rule_name                       = local.sa_security_rule_name
+  security_rule_name                       = local.sg_security_rule_name
   sg_resource_group_name                   = module.resourse_group.resourse_group_name
   sg_location                              = "East US"
   depends_on                               = [module.resourse_group]

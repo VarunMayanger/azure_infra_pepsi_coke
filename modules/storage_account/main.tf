4b12add9e1fcc20c2be3d02320fffc56747c954f
VarunMayanger <mrmayanger@gmail.com>
2023-08-29 12:18:55 +0530

    Update main.tf

--- modules/storage_account/main.tf
+++ modules/storage_account/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "sa" {
-  name                     = "${var.sa_base_name}sa"
+  name                     = "${var.sa_base_name}"
   resource_group_name      = "${var.sa_resource_group_name}"
   location                 = var.sa_location
   account_tier             = var.sa_account_tier

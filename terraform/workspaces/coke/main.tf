89f98b3ab7466cca7e1e4a75171ead9eeaa364b9
VarunMayanger <mrmayanger@gmail.com>
2023-08-29 12:32:44 +0530

    Update main.tf

--- terraform/workspaces/coke/main.tf
+++ terraform/workspaces/coke/main.tf
@@ -82,8 +82,8 @@ module "key_vault" {
   keyvault_name               = "cokekev23q3"
   location                    = local.rg_location
   resource_group_name         = module.resourse_group.resourse_group_name
-  name                        = "cokeKV23211"
-  value                       = "coke_value23211"
+  name                        = "cokeKV232181"
+  value                       = "coke_value232181"
 }
 
 # container registry

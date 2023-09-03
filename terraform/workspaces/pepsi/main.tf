d1f5c44135bae04e0837cce8e6c8df5f8d9ea144
VarunMayanger <mrmayanger@gmail.com>
2023-09-04 02:21:53 +0530

    Update main.tf

--- terraform/workspaces/pepsi/main.tf
+++ terraform/workspaces/pepsi/main.tf
@@ -83,7 +83,7 @@ module "key_vault" {
   keyvault_name               = "pepsikev23q32"
   location                    = local.rg_location
   resource_group_name         = module.resourse_group.resourse_group_name
-  name                        = "pepsi_sql"
+  name                        = "pepsisqlv1"
   value                       = module.passwords.sql_password
   depends_on                  = [module.passwords]
 }
